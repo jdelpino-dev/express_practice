2359fcbb3651621d3aa0830b8ce9e457eafaa07a
José Delpino <delpinoivivas@gmail.com>
2024-05-14 16:21:11 -0500

    refactor: Update /dogs route to use arrow function syntax and improve code formatting

--- secondRoute.js
+++ secondRoute.js
@@ -6,15 +6,15 @@ app.get("/", (req, res) => {
   return res.send("<h1>Welcome to the animal app running for good…</h1>");
 });
 
-app.get("/dogs", function (req, res) {
+app.get("/dogs", (req, res) => {
   return res.send("but what about these dogs???");
 });
 
 // this will never get matched
-app.get("/dogs", function (req, res) {
+app.get("/dogs", (req, res) => {
   return res.send("Dogs go brk brk");
 });
 
-app.listen(3300, function () {
+app.listen(3300, () => {
   console.log("App on port 3300");
 });
