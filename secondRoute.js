bb5ed0f6135f696d66568f46c5b71514e78446ce
José Delpino <delpinoivivas@gmail.com>
2024-05-14 16:28:06 -0500

    feat: Added chickens get and post request

--- secondRoute.js
+++ secondRoute.js
@@ -10,9 +10,19 @@ app.get("/dogs", (req, res) => {
   return res.send("but what about these dogs???");
 });
 
-// this will never get matched
+// this second /dogs route will never get matched, because
+// it is after the first / dogs route
 app.get("/dogs", (req, res) => {
   return res.send("Dogs go brk brk");
+});
+
+//  Beacause the are different verbs, both chickens routes will work!
+app.get("/chickens", function createChicken(req, res) {
+  return res.send("BOCK BOCK BOCK! It's a chicken and a get request!");
+});
+
+app.post("/chickens", function createChicken(req, res) {
+  return res.send("You created a chicken!");
 });
 
 app.listen(3300, () => {
