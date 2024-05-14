661a8fa6072f74c05a4834942e0ec84902cb555f
José Delpino <delpinoivivas@gmail.com>
2024-05-14 15:16:35 -0500

    refactor: Update firstServer.js to use ES6 import syntax for express

--- VideoCode/firstServer.js
+++ VideoCode/firstServer.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from "express";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000")
+app.listen(3300, () => {
+  console.log("My first Express server is running on port 3300!");
+  console.log("Get ready!");
 });
-
