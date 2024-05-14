df7eb2c9d9567e9f77126f31637982bf07344e5d
José Delpino <delpinoivivas@gmail.com>
2024-05-14 15:32:06 -0500

    Revert "refactor: Changed callbacks to arrow functions and added a home route"
    This reverts commit 580c47053012b925f9fe5103417ccc64f2c73ca1.

--- firstRoute.js
+++ firstRoute.js
@@ -2,14 +2,13 @@ import express from "express";
 
 const app = express();
 
-app.get("/", () => {
+app.get("/", (request, response) => {
   return response.send("Welcome to the animal app running for good…");
 });
 
-app.get("/dogs", () => {
+app.get("/dogs", (request, response) => {
   return response.send("Dogs go brk brk!");
 });
 
 app.listen(3300, () => {
   console.log("App on port 3300");
-});
