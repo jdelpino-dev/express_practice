fa79707ae97cb84439e228b0a9295db0ffe2331e
José Delpino <delpinoivivas@gmail.com>
2024-05-14 22:36:41 -0500

    refactor: Update server port number to 3300 in app.js

--- VideoCode/ExpressErrors/app.js
+++ VideoCode/ExpressErrors/app.js
@@ -61,6 +61,6 @@ app.use(function (err, req, res, next) {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(3300, () => {
+  console.log("Server running on port 3300");
 });
